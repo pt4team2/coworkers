8eff5a39c2c4e8a9854a7b6ba971efa442c82fc3
ilju jeon <lanace93@gmail.com>
2024-09-03 20:22:58 +0900

    이미지 파일을 다루는 2가지 방법

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../styles/globals.css';
+import Image from 'next/image';
+import dogImage from '../assets/images/dog.png';
 
 export default function Page() {
   return (
     <>
       <p className="flex bg-brand-primary text-sm">최상단</p>
+      <Image src={dogImage} alt="Vercel Logo" width={72} height={16} />
+      <Image src="/images/dog2.png" alt="Vercel Logo" width={72} height={16} />
     </>
   );
 }
