266e83309cda6642bc98b57a853ae4639a3ac405
hayuri1990 <hayuri1990@gmail.com>
2024-09-04 15:13:30 +0900

    feat: FormField 컴포넌트에 Tailwind CSS 적용

--- components/signup/FormField.tsx
+++ components/signup/FormField.tsx
@@ -22,14 +22,19 @@ const FormField = ({
   onIconClick,
 }: FormFieldProps) => {
   return (
-    <div>
-      <div>
+    <div className="flex flex-col gap-2">
+      <div className="relative">
         <input
           id={id}
           type={type}
           placeholder={placeholder}
           autoComplete="off"
           {...register(id)}
+          className={`placeholder:text-lg-regular h-12 w-85.75-custom rounded-xl border border-solid border-border-tertiary bg-background-secondary px-4 py-3.625-custom text-text-primary placeholder:text-text-default focus:outline-none md:w-115-custom ${
+            error
+              ? 'border-status-danger ring-1 ring-status-danger'
+              : 'focus:border-status-brand focus:ring-1 focus:ring-status-brand'
+          } `}
         />
         {trailingIcon && (
           <Image
@@ -38,10 +43,15 @@ const FormField = ({
             height={24}
             onClick={onIconClick}
             alt="비밀번호 표시 아이콘"
+            className="absolute right-4 top-1/2 -translate-y-1/2 transform cursor-pointer"
           />
         )}
       </div>
-      {error && <span>{error.message}</span>}
+      {error && (
+        <span className="text-md-medium text-status-danger">
+          {error.message}
+        </span>
+      )}
     </div>
   );
 };
