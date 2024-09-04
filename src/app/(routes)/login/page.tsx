fe334339856a705c4979bcae976916d937664767
hayuri1990 <hayuri1990@gmail.com>
2024-09-04 23:44:32 +0900

    feat: login 페이지에 Tailwind CSS 적용 및 반응형 구현

--- src/app/(routes)/login/page.tsx
+++ src/app/(routes)/login/page.tsx
@@ -41,13 +41,25 @@ export default function Login() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <div
+      className={`lg:mt-35-custom mt-6 flex flex-col items-center ${hasErrors ? 'md:mt-30-custom' : 'md:mt-25-custom'} ${hasErrors ? 'gap-6.5-custom md:gap-11.25-custom lg:gap-12.25-custom' : 'gap-6.25-custom md:gap-12.25-custom lg:gap-12'} `}
+    >
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex w-85.75-custom flex-col gap-10 md:w-115-custom lg:w-115-custom"
+      >
         <div>
-          <p>로그인</p>
-          <div>
-            <div>
-              <label htmlFor="email">이메일</label>
+          <p className="text-2xl-medium md:text-2xl-medium mb-6 text-center text-text-primary md:mb-20 lg:mb-20 lg:text-4xl">
+            로그인
+          </p>
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-3">
+              <label
+                htmlFor="email"
+                className="text-lg-medium text-text-primary"
+              >
+                이메일
+              </label>
               <FormField
                 id="email"
                 type="email"
@@ -56,8 +68,13 @@ export default function Login() {
                 error={errors.email}
               />
             </div>
-            <div>
-              <label htmlFor="password">비밀번호</label>
+            <div className="flex flex-col gap-3">
+              <label
+                htmlFor="password"
+                className="text-lg-medium text-text-primary"
+              >
+                비밀번호
+              </label>
               <FormField
                 id="password"
                 type={showPassword ? 'text' : 'password'}
@@ -69,27 +86,40 @@ export default function Login() {
               />
             </div>
           </div>
-          <Link href="#">비밀번호를 잊으셨나요?</Link>
+          <Link
+            href="#"
+            className="text-md-medium-alt md:text-lg-medium-alt lg:text-lg-medium-alt mt-3 flex flex-col text-end text-brand-primary underline"
+          >
+            비밀번호를 잊으셨나요?
+          </Link>
         </div>
-        <div>
-          <button type="submit" disabled={isSubmitting}>
+        <div className="flex flex-col gap-6">
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            className="text-lg-semibold rounded-xl bg-brand-primary py-3.5 text-text-primary"
+          >
             로그인
           </button>
-          <div>
-            <span>아직 계정이 없으신가요?</span>
-            <Link href="/signup">가입하기</Link>
+          <div className="text-md-medium md:text-lg-medium lg:text-lg-medium flex justify-center gap-3">
+            <span className="text-text-primary">아직 계정이 없으신가요?</span>
+            <Link href="/signup" className="text-status-brand underline">
+              가입하기
+            </Link>
           </div>
         </div>
       </form>
-      <div>
-        <div>
-          <div></div>
-          <span>OR</span>
-          <div></div>
-        </div>{' '}
-        <div>
-          <span>간편 로그인하기</span>
-          <div>
+      <div className="flex w-85.75-custom flex-col items-center gap-4 md:w-115-custom lg:w-115-custom">
+        <div className="flex w-full items-center text-text-primary">
+          <div className="h-px flex-grow bg-border-tertiary"></div>
+          <span className="text-lg-medium md:text-lg-regular lg:text-lg-regular mx-6 text-text-inverse">
+            OR
+          </span>
+          <div className="h-px flex-grow bg-border-tertiary"></div>
+        </div>
+        <div className="flex w-full justify-between">
+          <span className="text-lg-medium text-white">간편 로그인하기</span>
+          <div className="flex gap-4">
             <Link href="#">
               <Image
                 src={googleLogo}
