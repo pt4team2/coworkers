913dd4e633a99b0b1defe1cbb08b9055958323ce
김동규 <eastar385@naver.com>
2024-09-06 12:13:26 +0900

    feat: 리스트페이지 시작

---
+++ src/app/(routes)/list/page.tsx
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+
+export default function List(){
+    return (
+        <div>
+            <h3>할일</h3>
+            <div>5월 18일</div>
+            <div>법인 섭립 법인 동기 정기 주총 기타</div>
+            <>리스트</>
+        </div>
+
+    );
+}
