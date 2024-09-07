6bc763f7b4662d701d7247bb6097e0346b70d763
hayuri1990 <hayuri1990@gmail.com>
2024-09-08 07:15:25 +0900

    refactor: 필드 데이터 반복 코드 useResetPasswordFieldData로 리팩토링, 불필요한 코드 제거

--- src/app/(routes)/resetPassword/page.tsx
+++ src/app/(routes)/resetPassword/page.tsx
@@ -6,10 +6,11 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { RESET_PASSWORD_SCHEMA } from '@/utils/schema';
 import { ResetPassword } from '@/types/auth';
 import FormField from '@/components/auth/FormField';
-import visibility_on from '@/assets/icons/visibility_on.svg';
-import visibility_off from '@/assets/icons/visibility_off.svg';
+import { useResetPasswordFieldData } from '@/hooks/useFormFieldData';
 
 export default function ResetPasswordPage() {
+  const resetPasswordFields = useResetPasswordFieldData();
+
   const {
     register,
     handleSubmit,
@@ -18,19 +19,6 @@ export default function ResetPasswordPage() {
     resolver: yupResolver(RESET_PASSWORD_SCHEMA),
     mode: 'onChange',
   });
-
-  // 비밀번호 표시
-  const [showpassword, setShowpassword] = useState(false);
-  const togglepasswordVisibility = () => {
-    setShowpassword(!showpassword);
-  };
-
-  // 비밀번호 확인 표시
-  const [showpasswordConfirmation, setShowpasswordConfirmation] =
-    useState(false);
-  const togglepasswordConfirmationVisibility = () => {
-    setShowpasswordConfirmation(!showpasswordConfirmation);
-  };
 
   // error 메시지 여부 확인
   const hasErrors = Object.keys(errors).length > 0;
@@ -52,42 +40,22 @@ export default function ResetPasswordPage() {
             비밀번호 재설정
           </p>
           <div className="flex flex-col gap-6">
-            <div className="flex flex-col gap-3">
-              <label
-                htmlFor="password"
-                className="text-lg-medium text-text-primary"
-              >
-                새 비밀번호
-              </label>
-              <FormField
-                id="password"
-                type={showpassword ? 'text' : 'password'}
-                placeholder="비밀번호 (숫자, 영문, 특수문자, 8자 이상)를 입력해주세요."
-                trailingIcon={showpassword ? visibility_off : visibility_on}
-                onIconClick={togglepasswordVisibility}
-                register={register}
-                error={errors.password}
-              />
-            </div>
-            <div className="flex flex-col gap-3">
-              <label
-                htmlFor="password"
-                className="text-lg-medium text-text-primary"
-              >
-                비밀번호 확인
-              </label>
-              <FormField
-                id="passwordConfirmation"
-                type={showpasswordConfirmation ? 'text' : 'password'}
-                placeholder="새 비밀번호를 다시 한번 입력해주세요."
-                trailingIcon={
-                  showpasswordConfirmation ? visibility_off : visibility_on
-                }
-                onIconClick={togglepasswordConfirmationVisibility}
-                register={register}
-                error={errors.passwordConfirmation}
-              />
-            </div>
+            {resetPasswordFields.map((field) => (
+              <div key={field.id} className="flex flex-col gap-3">
+                <label
+                  htmlFor={field.id}
+                  className="text-lg-medium text-text-primary"
+                >
+                  {field.id === 'password' ? '새 비밀번호' : '비밀번호 확인'}
+                </label>
+                <FormField
+                  key={field.id}
+                  {...field}
+                  register={register}
+                  error={errors[field.id as keyof ResetPassword]}
+                />
+              </div>
+            ))}
           </div>
         </div>
         <div className="flex flex-col gap-6">
