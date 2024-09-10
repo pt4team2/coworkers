e18d21d06644fb4fe5fcc891fc56863742e5f42f
JeongYuseung <dbtmd0925@gmail.com>
2024-09-11 06:20:41 +0900

    feat/베스트 게시글, 게시글카드

---
+++ src/components/pages/boards/articleCard/ArticleCard.tsx
@@ -0,0 +1,106 @@
+import Image from 'next/image';
+import type { StaticImageData } from 'next/image';
+import LikeIcon from '@/assets/icons/ic_heart.svg';
+import MemberIcon from '@/assets/icons/ic_member.svg';
+import BoardImage from '@/assets/images/img_boardtest.png';
+import IcKebeb from '@/assets/icons/ic_kebab.svg';
+
+interface Writer {
+  name: string;
+}
+
+interface Article {
+  id: number;
+  title: string;
+  image: string | StaticImageData | null;
+  createdAt: string;
+  updatedAt: string;
+  writer: Writer;
+  content: string;
+  likeCount: number;
+  isLiked: boolean;
+}
+
+// 임시 데이터
+const mockArticle: Article = {
+  id: 1,
+  title: '자유게시판에 질문을 어쩌구',
+  image: BoardImage,
+  createdAt: '2024-07-25',
+  updatedAt: '2024-07-25',
+  writer: {
+    name: '우지은',
+  },
+  content: '질문을 올려볼까요?',
+  likeCount: 9999,
+  isLiked: false,
+};
+
+const ArticleCard = () => {
+  const board = mockArticle; // 임시 데이터
+
+  return (
+    <div
+      className="relative w-[343px] h-auto p-[24px_16px_16px] gap-4 border rounded-[12px]"
+      style={{
+        backgroundColor: 'var(--color-background-secondary)',
+        borderColor: '#334155',
+      }}
+    >
+
+      <div className="flex">
+        <div className="flex-1 flex flex-col justify-between">
+          <div
+            className="text-md-medium-alt"
+            style={{ color: 'var(--color-text-secondary)' }}
+          >
+            {board.title}
+          </div>
+          <div
+            className="text-xs-medium"
+            style={{ color: 'var(--color-text-disabled)' }}
+          >
+            {new Date(board.createdAt).toLocaleDateString()}
+          </div>
+        </div>
+        {board.image && (
+          <div className="border-[1px] border-[#475569] rounded-[8px] ml-[23px] mb-[10px]">
+            <Image
+              src={board.image}
+              alt={board.title}
+              className="rounded-[8px]"
+              width={64}
+              height={64}
+            />
+          </div>
+        )}
+      </div>
+
+      <div className="flex justify-between items-center mt-4">
+        <div className="flex items-center">
+          <Image
+            src={MemberIcon}
+            alt="작성자 이미지"
+            className="rounded-full"
+            width={32}
+            height={32}
+          />
+          <span className="text-xs-medium ml-3">{board.writer.name}</span>
+        </div>
+
+        <div className="flex items-center">
+          <Image src={LikeIcon} alt="likeIcon" width={18} />
+          <span
+            className="text-xs-regular ml-2"
+            style={{ color: 'var(--color-text-disabled)' }}
+          >
+            {board.likeCount}
+          </span>
+          <Image src={IcKebeb} alt="kebab icon" width={16} height={16} className="ml-2" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ArticleCard;
