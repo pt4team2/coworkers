57916b8da8eced4f202b5ddbbdb0823cbc88020a
Ayoung <ay0109001@gmail.com>
2024-09-13 21:13:43 +0900

    feat: queryClientProvider 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import '../styles/globals.css';
 import Header from '@/components/header/Header';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
-    <html lang="en">
-      <body>
-        <main>{children}</main>
-      </body>
-    </html>
+    <QueryClientProvider client={queryClient}>
+      <html lang="en">
+        <body>
+          <main>{children}</main>
+        </body>
+      </html>
+    </QueryClientProvider>
   );
 }
