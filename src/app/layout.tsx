f444794734f7ac77268c9aedc43c7f9cd517ecb5
hayuri1990 <hayuri1990@gmail.com>
2024-09-14 19:07:55 +0900

    feat: next-auth의 SessionProvider 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/globals.css';
 import Header from '@/components/header/Header';
+import Providers from '@/components/auth/Providers';
 
 export default function RootLayout({
   children,
@@ -10,7 +11,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <main>{children}</main>
+        <Providers>
+          <header>
+            <Header />
+          </header>
+          <main className="m-auto max-w-[1200px] px-4 md:px-6 lg:px-0">
+            {children}
+          </main>
+        </Providers>
       </body>
     </html>
   );
