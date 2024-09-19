adf708d0c14ddfe56a45a20b153459844e5712c2
yellowjang <166773598+yellowjang@users.noreply.github.com>
2024-09-19 17:11:07 +0900

    feat: env 파일생성, 데이터 페칭 훅, 파일 수정 (#14)
    * feat: queryClientProvider 설정
    
    * feat: 드롭다운 화면 밖 클릭 모달 닫히기 && axios && useGroup 훅 추가
    
    * fix: lang 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,33 @@
+'use client';
+
 import React from 'react';
 import '../styles/globals.css';
 import Header from '@/components/header/Header';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
 import Providers from '@/components/auth/Providers';
+
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
-    <html lang="en">
-      <body>
-        <Providers>
-          <header>
-            <Header />
-          </header>
+    <QueryClientProvider client={queryClient}>
+      <html lang="ko">
+        <body>
+          <Providers>
+            <header>
+              <Header/>
+            </header>
           <main className="m-auto max-w-[1200px] px-4 md:px-6 lg:px-0">
             {children}
           </main>
-        </Providers>
-      </body>
-    </html>
+          </Providers>
+        </body>
+      </html>
+    </QueryClientProvider>
   );
 }
