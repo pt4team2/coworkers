173fd656da62fe5af58578a08feed041481940b8
hayuri1990 <hayuri1990@gmail.com>
2024-09-30 10:44:16 +0900

    feat: 타입 추가 및 변경

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,17 +1,50 @@
 import NextAuth from 'next-auth';
 
-declare module 'next-auth' {
+export declare module 'next-auth' {
   interface Session {
     user: {
       id: number;
       nickname: string;
-      email: string;
-      image: string;
+      email?: string;
+      image: string | null;
       teamId: string;
       createdAt: string;
       updatedAt: string;
     };
     accessToken: string;
-    refreshToken: string;
+    refreshToken: string | undefined;
+    accessTokenExpires: number;
+    error?: string;
+  }
+
+  interface User {
+    id: number;
+    nickname: string;
+    email?: string;
+    image: string | null;
+    teamId: string;
+    createdAt: string;
+    updatedAt: string;
+    accessToken: string;
+    refreshToken: string | undefined;
+    accessTokenExpires: number;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    user: {
+      id: number;
+      nickname: string;
+      email?: string;
+      image: string | null;
+      teamId: string;
+      createdAt: string;
+      updatedAt: string;
+    };
+    accessToken: string;
+    refreshToken: string | undefined;
+    accessTokenExpires: number;
+    error?: string;
   }
 }
