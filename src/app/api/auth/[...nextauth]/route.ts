ede9fe751a3d39481a3f181e5e81e315b6918b65
hayuri1990 <hayuri1990@gmail.com>
2024-09-30 10:44:36 +0900

    feat: 자체 로그인 시 accessToken 만료 10분 전 갱신 기능 적용

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,71 +1,9 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import { publicAxiosInstance } from '@/app/api/auth/axiosInstance';
+import NextAuth from 'next-auth';
+import { getOptions } from '@/nextAuthOptions';
+import { NextRequest } from 'next/server';
 
-const handler = NextAuth({
-  debug: true,
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        email: { label: 'Email', type: 'text' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials) {
-        console.log('authorize 함수 실행 중', credentials);
-        try {
-          // API 호출
-          const result = await publicAxiosInstance.post('/auth/signIn', {
-            email: credentials?.email,
-            password: credentials?.password,
-          });
-
-          // 응답 데이터 처리
-          const user = await result.data;
-
-          // 사용자 인증 성공 처리
-          if (user) {
-            console.log('-----------');
-            console.log(user);
-            return user;
-          } else {
-            console.log('-----------');
-            console.log(null);
-            return null;
-          }
-        } catch (error) {
-          // 오류 처리
-          console.log('-----------');
-          console.error('Error during login:', error);
-          return null;
-        }
-      },
-    }),
-  ],
-  session: {
-    strategy: 'jwt',
-    maxAge: 60 * 60 * 24, // 세션 만료 시간: 24시간
-  },
-  pages: {
-    signIn: '/login',
-  },
-  callbacks: {
-    signIn: async () => {
-      return true;
-    },
-    async jwt({ token, user }) {
-      if (user) {
-        return { ...token, ...user };
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (token) {
-        session = token as any;
-      }
-      return session;
-    },
-  },
-} as NextAuthOptions);
+const handler = async (req: NextRequest, context: any) => {
+  return await NextAuth(req, context, getOptions(req.clone()));
+};
 
 export { handler as GET, handler as POST };
