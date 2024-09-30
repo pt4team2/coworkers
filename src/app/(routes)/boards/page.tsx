c226d9313c079a4e6f7e1a7f6fe74b23677bf7fb
JeongYuseung <dbtmd0925@gmail.com>
2024-09-30 11:59:08 +0900

    feat/게시물작성페이지 연결버튼 연결

--- src/app/(routes)/boards/page.tsx
+++ src/app/(routes)/boards/page.tsx
@@ -1,23 +1,76 @@
-'use client';
+"use client";
 
-import { useRouter } from 'next/navigation';
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import Dropdown from '@/components/pages/boards/dropdown/Dropdown';
+import BestArticleCard from '@/components/pages/boards/bestArticleCard/BestArticleCard';
+import ArticleCard from '@/components/pages/boards/articleCard/ArticleCard';
+import SearchForm from '@/components/pages/boards/searchForm/SearchForm';
+import FloatingButton from '@/components/pages/boards/flaotingButton/FloatingButton';
+import RightArrowIcon from '@/assets/icons/ic_rightArrow.svg';
 
-const FloatingButton = () => {
-  const router = useRouter();
+const BoardPage = () => {
+  const [cardCount, setCardCount] = useState(1);
 
-  const handleClick = () => {
-    router.push('/addboards');
+  const handleResize = () => {
+    if (window.innerWidth >= 1200) {
+      setCardCount(3);
+    } else if (window.innerWidth >= 744) {
+      setCardCount(2);
+    } else {
+      setCardCount(1);
+    }
   };
 
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const bestArticles = [1, 2, 3]; // 임시 카드 배열
+
   return (
-    <button
-      onClick={handleClick}
-      className="fixed bottom-6 right-0 flex items-center justify-center w-24 h-12 bg-[#10B981] rounded-full z-50 text-lg-semibold"
-    >
-      <span className="mr-2 text-xl">+</span>
-      글쓰기
-    </button>
+    <div className="px-4 py-8 bg-[#0F172A] min-h-screen md:px-6 md:py-10 lg:py-10">
+      <h1 className="text-2lg-bold mb-6">자유게시판</h1>
+      <SearchForm onSearch={(term) => console.log(term)} placeholder="검색어를 입력해주세요" />
+
+      <div className="mt-6">
+      <div className="flex justify-between items-center mb-6">
+        <h2 className="text-lg-medium">베스트 게시글</h2>
+        <button className="flex items-center">
+            <span className="text-[#94A3B8] text-xs-regular">더보기</span>
+            <Image src={RightArrowIcon} alt="오른쪽 화살 아이콘" width={16} height={16} className="ml-[2px]" />
+        </button>
+      </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {bestArticles.slice(0, cardCount).map((article, index) => (
+            <BestArticleCard key={index} />
+          ))}
+        </div>
+      </div>
+
+      <div className="border-t border-[#F8FAFC1A] my-8"></div>
+
+      <div>
+        <div className="flex justify-between items-center mb-4 relative">
+          <h2 className="text-white text-lg-regular">게시글</h2>
+          <div className="relative z-10">
+            <Dropdown onSelect={(option) => console.log(option)} />
+          </div>
+        </div>
+        
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 z-0">
+          <ArticleCard />
+          <ArticleCard />
+          <ArticleCard />
+          <ArticleCard />
+        </div>
+      </div>
+      <FloatingButton />
+    </div>
   );
 };
 
-export default FloatingButton;
+export default BoardPage;
---
+++ src/components/pages/boards/flaotingButton/FloatingButton.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import PlusIcon from '@/assets/icons/ic_plus.svg';
+
+const FloatingButton = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/addboards');
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className="fixed bottom-6 right-6 flex items-center justify-center w-[104px] h-12 bg-[#10B981] rounded-full z-50 text-lg-semibold"
+    >
+      <Image src={PlusIcon} alt="이미지 등록" className="w-4 h-4 mr-1"/>
+      글쓰기
+    </button>
+  );
+};
+
+export default FloatingButton;
