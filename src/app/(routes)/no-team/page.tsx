76e45cebab84543c751f5d669a01e410fa7d9d14
yellowjang <166773598+yellowjang@users.noreply.github.com>
2024-10-02 11:47:06 +0900

    feat: 마이 히스토리 페이지 퍼블리싱

--- src/app/(routes)/no-team/page.tsx
+++ src/app/(routes)/no-team/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import EmptyImg from '@/assets/images/img_empty.svg';
+import Link from 'next/link';
 
 export default function Page() {
   return (
@@ -17,13 +18,17 @@ export default function Page() {
       <p className="text-md-medium text-center text-text-default">
         팀을 생성하거나 팀에 참여해보세요.
       </p>
-      <div className="mt-12 flex flex-col gap-2">
-        <button className="text-lg-semibold rounded-[12px] border border-solid border-brand-primary bg-brand-primary px-[61px] py-[15.5px] text-text-inverse">
-          팀 생성하기
-        </button>
-        <button className="text-lg-semibold rounded-[12px] border border-solid border-brand-primary bg-background-primary px-[61px] py-[15.5px] text-text-inverse">
-          팀 참여하기
-        </button>
+      <div className="mt-12 flex flex-col gap-2 md:mt-20 lg:mt-20">
+        <Link href="/create-team">
+          <button className="text-lg-semibold rounded-[12px] border border-solid border-brand-primary bg-brand-primary px-[61px] py-[15.5px] text-text-inverse">
+            팀 생성하기
+          </button>
+        </Link>
+        <Link href="/join-team">
+          <button className="text-lg-semibold rounded-[12px] border border-solid border-brand-primary bg-background-primary px-[61px] py-[15.5px] text-text-inverse">
+            팀 참여하기
+          </button>
+        </Link>
       </div>
     </div>
   );
