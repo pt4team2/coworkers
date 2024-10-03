07aa406211ffdf583d73ab732f018bd753ff510d
hayuri1990 <hayuri1990@gmail.com>
2024-10-04 05:47:10 +0900

    chore: 사용하지 않는 useSession import 및 불필요한 코드 삭제

--- src/components/header/Profile.tsx
+++ src/components/header/Profile.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import Image from 'next/image';
 import IcProfile from 'src/assets/icons/ic_profile.svg';
 // import { teamMockData } from '@/data/mockData';
-import { useSession } from 'next-auth/react';
 import ProfileDropdown from './ProfileDropdown';
 import { IUser } from '@/types/user';
 
@@ -14,7 +13,7 @@ interface ProfileProps {
 
 export default function ({ user }: ProfileProps) {
   if (!user) {
-    return <div>로그인이 필요합니다.</div>; // 유저 정보가 없을 때
+    return <div></div>; // 유저 정보가 없을 때
   }
 
   return (
