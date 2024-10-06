501dfb6e26c2de4ae4870246fd6ed161c8970d47
김동규 <eastar385@naver.com>
2024-10-07 00:18:15 +0900

    feat: 모달컴포넌트 최종

--- src/app/(routes)/list/example/page.tsx
+++ src/app/(routes)/list/example/page.tsx
@@ -2,18 +2,21 @@
 
 import { useState } from 'react';
 import PopupOneButton from '@/components/Modal/PopupOneButton';
-import ModalProfile from '@/components/modal/ProfileModal';
 import ModalDanger from '@/components/Modal/ModalDanger';
 import ModalDangerLogout from '@/components/Modal/ModalDangerLogout';
 import ModalTwoButton from '@/components/Modal/ModalTwoButton';
 import ModalOneButton from '@/components/Modal/ModalOneButton';
 import ModalTwoButtonPassword from '@/components/Modal/ModalTwoButtonPassword';
 import ModalToDo from '@/components/Modal/ModalToDo';
+import ModalNewList from '@/components/Modal/ModalNewList';
 export default function Example() {
   const [isModalOpen, setIsModalOpen] = useState(true);
   return (
     <div className="bg-background-inverse">
-      <ModalToDo isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+      <ModalOneButton
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
     </div>
   );
 }
--- src/components/Modal/ModalOneButton.tsx
+++ src/components/Modal/ModalOneButton.tsx
@@ -15,12 +15,15 @@ const ModalOneButton = ({ isOpen, onClose }: ModalProps) => {
       <button className="absolute right-3 top-3">
         <Image src={CloseIcon} alt="CloseIcon" width={24} height={24} />
       </button>
-      <div className="mx-auto mb-8 mt-12 flex w-[280px] flex-col items-center justify-center">
-        <div className="flex h-[79px] flex-col justify-between md:h-[83px] lg:h-[83px]">
+      <div className="mx-auto mb-8 mt-12 flex h-[151px] w-[280px] flex-col items-center justify-between md:h-[155px] lg:h-[155px]">
+        <div className="flex h-[79px] w-full flex-col justify-between md:h-[83px] lg:h-[83px]">
           <h2 className="text-lg-medium text-center text-text-primary">
             할 일 목록
           </h2>
-          <input className="h-[79px] rounded-xl border border-border-primary bg-background-secondary md:h-[83px] lg:h-[83px]" />
+          <input
+            className="h-11 w-full rounded-xl border border-solid border-border-primary bg-background-secondary p-4 placeholder:text-text-default focus:border-none focus:outline-none focus:ring-1 focus:ring-[#F8FAFC] md:h-12 lg:h-12"
+            placeholder="목록 명을 입력해주세요."
+          />
         </div>
         <button
           className="px-auto py-auto h-[47px] w-full rounded-xl bg-brand-primary text-text-inverse"
